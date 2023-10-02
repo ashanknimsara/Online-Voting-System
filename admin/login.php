ee6c80779fc3696cb1448a7cb8cca835d0a7efb0
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-10-02 22:35:33 +0530

    captcha implemented

--- admin/login.php
+++ admin/login.php
@@ -2,6 +2,16 @@
 	session_start();
 	include 'includes/conn.php';
 
+	//recaptcha
+	$captcha = $_POST['g-recaptcha-response'];
+	$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcdTW4oAAAAAAhhAo2Fbn5yYBqkrXBMtyBLApoP&response=".$captcha);
+	$decoded_response = json_decode($response, true);
+	if(!$decoded_response['success']){
+		$_SESSION['error'] = 'Invalid captcha. Please try again.';
+		header('location: index.php');//if captcha not validate again ridirect to index page
+		exit;
+	}//end of recaptcha
+
 	if(isset($_POST['login'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
