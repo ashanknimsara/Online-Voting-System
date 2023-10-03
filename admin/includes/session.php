884d03c4643778a95824f616ee7b596086567d13
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-10-03 22:04:40 +0530

    fix login

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,4 +1,12 @@
 <?php
+ob_start(); 
+session_set_cookie_params([
+    'lifetime' => 0,   // 0 means until the browser is closed
+    'path'     => '/',
+    'domain'   => '',
+    'secure'   => false, // If you use HTTPS, set this to true
+    'httponly' => true   // Enable HTTP Only flag
+]);
 	session_start();
 	include 'includes/conn.php';
 
@@ -9,5 +17,7 @@
 	$sql = "SELECT * FROM admin WHERE id = '".$_SESSION['admin']."'";
 	$query = $conn->query($sql);
 	$user = $query->fetch_assoc();
+
+	ob_end_flush(); 
 	
 ?>
--- admin/login.php
+++ admin/login.php
@@ -2,38 +2,31 @@
 	session_start();
 	include 'includes/conn.php';
 
-	// //recaptcha
-	// $captcha = $_POST['g-recaptcha-response'];
-	// $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcdTW4oAAAAAAhhAo2Fbn5yYBqkrXBMtyBLApoP&response=".$captcha);
-	// $decoded_response = json_decode($response, true);
-	// if(!$decoded_response['success']){
-	// 	$_SESSION['error'] = 'Invalid captcha. Please try again.';
-	// 	header('location: index.php');//if captcha not validate again ridirect to index page
-	// 	exit;
-	// }//end of recaptcha
-
-	if (isset($_POST['login'])) {
+	if(isset($_POST['login'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-	
+
 		$sql = "SELECT * FROM admin WHERE username = '$username'";
 		$query = $conn->query($sql);
-	
-		if ($query->num_rows < 1) {
-			$_SESSION['error'] = 'Invalid Admin Credentials';
-		} else {
+
+		if($query->num_rows < 1){
+			$_SESSION['error'] = 'Invalid Credentials';
+		}
+		else{
 			$row = $query->fetch_assoc();
-			if (password_verify($password, $row['password'])) {
+			if(password_verify($password, $row['password'])){
 				$_SESSION['admin'] = $row['id'];
-				header('location: home.php'); // Redirect to the home page on successful login
-				exit;
-			} else {
-				$_SESSION['error'] = 'Invalid Admin Credentials';
+			}
+			else{
+				$_SESSION['error'] = 'Invalid Credentials';
 			}
 		}
-	} else {
+		
+	}
+	else{
 		$_SESSION['error'] = 'Input admin credentials first';
 	}
-	
+
 	header('location: index.php');
-?>	
+
+?>
