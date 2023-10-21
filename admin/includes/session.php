4e8fc61607c0f09154b1ef22d99b408e8e40c3ea
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-10-21 12:56:55 +0530

    session expire

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,23 +1,34 @@
 <?php
-ob_start(); 
-session_set_cookie_params([
-    'lifetime' => 0,   // 0 means until the browser is closed
-    'path'     => '/',
-    'domain'   => '',
-    'secure'   => false, // If you use HTTPS, set this to true
-    'httponly' => true   // Enable HTTP Only flag
-]);
-	session_start();
-	include 'includes/conn.php';
+session_start();
+include 'includes/conn.php';
 
-	if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
-		header('location: index.php');
-	}
+// Check if the user is logged in, otherwise, redirect to the login page
+if (!isset($_SESSION['admin']) || empty($_SESSION['admin'])) {
+    header('location: index.php');
+    exit;
+}
 
-	$sql = "SELECT * FROM admin WHERE id = '".$_SESSION['admin']."'";
-	$query = $conn->query($sql);
-	$user = $query->fetch_assoc();
+// Check if there is a last activity timestamp in the session
+if (isset($_SESSION['last_activity'])) {
+    // Check if the user has been inactive for more than 1800 seconds
+    $inactive_seconds = 1800;
+    $current_time = time();
+    $last_activity_time = $_SESSION['last_activity'];
 
-	ob_end_flush(); 
-	
+    if ($current_time - $last_activity_time > $inactive_seconds) {
+        // Expire the session
+        session_unset();
+        session_destroy();
+        header('location: index.php');
+        exit;
+    }
+}
+
+// Update the last activity timestamp
+$_SESSION['last_activity'] = time();
+
+// Retrieve user data
+$sql = "SELECT * FROM admin WHERE id = '".$_SESSION['admin']."'";
+$query = $conn->query($sql);
+$user = $query->fetch_assoc();
 ?>
